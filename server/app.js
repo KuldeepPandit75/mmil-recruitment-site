cee30e4822b46c2a3e88e26eefefd7f9e74a4cc1
nandini924 <nandinimishra105@gmail.com>
2026-02-12 14:41:15 +0530

    Resolved merge conflicts in app.js and designModel

--- server/app.js
+++ server/app.js
@@ -4,14 +4,11 @@ import morgan from "morgan";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import authRouter from "./routes/authRoute.js";
 import profileRouter from "./routes/profileUpdateRoute.js";
-import adminRouter from "./routes/applicationsRoute.js";
-import applications from "./routes/applicationsRoute.js";
 
 import webDevRouter from "./routes/webDevRoute.js";
 import technicalRouter from "./routes/technicalRoute.js";
 import designRouter from "./routes/designRoute.js";
 
-import adminRouter from "./routes/applicationsRoute.js";
 import applications from "./routes/applicationsRoute.js";
 
 const app = express();
@@ -22,7 +19,6 @@ app.use(morgan("dev"));
 
 // Routes can go here...
 
-<<<<<<< HEAD
 app.use("/api/auth", authRouter);
 
 app.use("/api/webdev", webDevRouter);
@@ -30,21 +26,13 @@ app.use("/api/technical", technicalRouter);
 app.use("/api/design", designRouter);
 
 app.use("/api/auth", profileRouter);
-app.use("/api/auth", adminRouter);
 
-app.use("/api/admin", applications);
-=======
-app.use("/api/auth",authRouter)
-app.use("/api/auth",profileRouter)
-app.use("/api/auth",adminRouter)
-
-app.use("/api/admin",applications)
->>>>>>> 0e05e0ea9d102dc28ecf0a9404065dfc67c6871b
+app.use("/api/admin", applicationsRouter);
 
 app.use("/uploads", express.static("uploads"));
 
 // example --> app.use("/api", indexRoutes);
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("server started");
 });
 app.use((req, res, next) => {
